ca8d1f176015ed18c79b91d53390661babb0a34b
jude abouhajar <judeabouhajar@judes-air.home>
2023-11-12 21:35:09 +0000

    final

---
+++ backend/models/Transaction.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+//creating schema
+const Schema = mongoose.Schema;
+
+//defining new instance of the schema
+const transactionSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    //refernce to the user
+    user: {
+        type: mongoose.Types.ObjectId, 
+        ref: "User",
+        required: true,
+    }, //one user per transaction 
+
+});
+
+export default mongoose.model("Transaction", transactionSchema);
